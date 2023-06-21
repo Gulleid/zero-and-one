e2f6b424795ab92c403dbf7cc3692e0f02939b36
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-21 09:32:55 +0300

    sero and one

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+function zeroAndOne(input) {
+    if (input === 'zero'  && input !== 0) {
+      return '0';
+    } else if (input === 'one' && input !== 'ones') {
+      return '1';
+    } else {
+      return input;
+    }
+  }
+  
+  console.log(zeroAndOne('zero','zero',));
+  console.log(zeroAndOne('one','one')); 
+  
